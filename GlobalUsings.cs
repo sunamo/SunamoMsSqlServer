563ae3e5fc4613e7a04c08d88348d78d3ee03cac
sunamo.cz <radek.jancik@sunamo.cz>
2025-06-07 20:26:22 +0200

    25.6.7.1 Removed unused method parameters

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -13,3 +13,4 @@ global using System;
 global using ILogger = Microsoft.Extensions.Logging.ILogger;
 global using Microsoft.Extensions.Logging;
 global using NullLogger = Microsoft.Extensions.Logging.Abstractions.NullLogger;
+global using SunamoMsSqlServer._sunamo;
