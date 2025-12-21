5992e42f3bf40f7642821a173a90c336a1daf294
Radek Jancik <radek.jancik@usys.cz>
2025-12-21 21:22:34 +0100

    25.12.21.1 feat: Added net10.0 to TargetFrameworks

--- SunamoMsSqlServer/GlobalUsings.cs
+++ SunamoMsSqlServer/GlobalUsings.cs
@@ -1,5 +1,3 @@
-// EN: Variable names have been checked and replaced with self-descriptive names
-// CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
 global using SunamoMsSqlServer.Helpers;
 global using SunamoMsSqlServer.Services;
 global using SunamoMsSqlServer._public;
--- SunamoMsSqlServer/_sunamo/ThrowEx.cs
+++ SunamoMsSqlServer/_sunamo/ThrowEx.cs
@@ -1,6 +1,7 @@
+namespace SunamoMsSqlServer._sunamo;
+
 // EN: Variable names have been checked and replaced with self-descriptive names
 // CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
-﻿namespace SunamoMsSqlServer._sunamo;
 internal class ThrowEx
 {
     internal static void NotImplementedMethod()
